d2d8f80c10606a9a0dde47aa71266da9f28e72c5
Jose Annunziato <jannunzi@gmail.com>
2026-01-20 13:20:40 -0500

    fix default landing pages

--- app/(kambaz)/page.tsx
+++ app/(kambaz)/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Kambaz() {
-  return (
-    <div>
-      <h1>Welcome to Kambaz!</h1>
-    </div>
-  );
+  redirect("/account/signin");
 }
